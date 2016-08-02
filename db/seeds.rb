86ec1ac560d7522a5f0a85638f897b4dd6230a78
houtaiw61@gmail.com <houtaiw61@gmail.com>
2016-08-02 19:37:50 -0400

    fixed footer so it will always be on bottom page even if the body is small. changed buttons to uniform style

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ user2 = User.create({
 project1 = Project.create!({
   owner_id: User.find_by(name: "Natalie").id,
   name: "Fred's not here",
-  description: "So good",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   end_date: Date.today+2.weeks,
   funding_goal: 1000,
   media_url: "http://placekitten.com/200/300"
@@ -32,7 +32,7 @@ project1 = Project.create!({
 Project.create!({
   owner_id: User.find_by(name: "Natalie").id,
   name: "Another",
-  description: "So good",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   end_date: Date.today+2.weeks,
   funding_goal: 1000,
   media_url: "http://placekitten.com/200/300"
@@ -41,7 +41,7 @@ Project.create!({
 Project.create!({
   owner_id: User.find_by(name: "Natalie").id,
   name: "Project 2",
-  description: "So good",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   end_date: Date.today+2.weeks,
   funding_goal: 1000,
   media_url: "http://placekitten.com/200/300"
