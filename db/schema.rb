83db0a80248cf1b7d8874479fd17faf3df3d8510
houtaiw61@gmail.com <houtaiw61@gmail.com>
2016-08-22 18:14:54 -0400

    added postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160504190732) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pledges", force: :cascade do |t|
     t.integer  "backer_id"
     t.integer  "reward_id"
@@ -49,6 +52,6 @@ ActiveRecord::Schema.define(version: 20160504190732) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
